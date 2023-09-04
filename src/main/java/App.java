82d54d41b71053805e1176724047c9dc85c39042
Thabo Nyoni <t.nyoni@corecomtechacademy.co.uk>
2023-09-04 15:05:03 +0100

    changed main to use computer as opponent

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) {
         Player x = new RealPlayer("tha");
-        Player y = new RealPlayer("kfhdjd");
-        //Player y = new Computer();
+        //Player y = new RealPlayer("kfhdjd");
+        Player y = new Computer();
         Game z = new Game(x,y, 3);
         z.playGame();
     }
